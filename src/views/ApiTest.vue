205b34513e8f7117c9deafcb7d4a27236f8b2732
junhyeon0325 <jjh990325@gmail.com>
2025-09-02 12:11:10 +0900

    장준현_20250902 1210_3교시 첫 커밋

---
+++ src/views/ApiTest.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      msg: "API Test",
+    };
+  },
+  mounted() {
+    axios //
+      .post({ url: "api/productList", data: {} })
+      .then((result) => console.log(result));
+  },
+};
+</script>
+
+<style>
+/*  */
+</style>
